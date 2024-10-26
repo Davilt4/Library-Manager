8c02147df9a8cb0f2bac7bee8880eb040746d1bc
oiclai <claradealcantara285@gmail.com>
2024-10-25 23:54:33 -0300

    :construction: feat: add object-oriented classes

---
+++ src/models/livro.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+from usuario import *
+
+class Book:
+    def __init__ (self, titulo, autor, ano, id_livro):
+        self.titulo = titulo; self.autor = autor; self.id_livro = id_livro; self.ano = ano
+        self.disponivel = True; self.user = None; self.data_emprestimo = None 
+        # inicialmente no sistema o livro nao foi emprestado (sem user), nao apresentando data de emprestimo (ele vai ganhando um historico de data de emprestimos)
+
+    # user == instancia da class User_Student -> precisa de uma garantia melhor d/que vem da User_Student
+    def emprestado(self, user, data_emprestimo): 
+        self.user = user 
+        if not self.disponivel:
+            print("ERROR: Livro já emprestado.")
+            return # impossibilita emprestimo se ja estiver emprestado
+        self.data_emprestimo = datetime.strptime(data_emprestimo, "%d/%m/%Y");
+        self.disponivel = False; # livro emprestado
+
+    def devolvido(self, data_devolucao):
+        if self.data_emprestimo is None:
+            print("ERROR: esse não foi o livro emprestado.")
+            return
+        self.data_devolucao = datetime.strptime(data_devolucao, "%d/%m/%Y")
+        self.maxdata_devolucao = self.data_emprestimo + timedelta(days=30)
+
+        if self.data_devolucao > self.maxdata_devolucao and self.user: 
+            self.user.credito_emprestimo -= 1  # reduz o credito
+        self.disponivel = True; self.user = None; # remove a referencia do usuario ja q o livro ja foi devolvido
---
+++ src/models/usuario.py
@@ -0,0 +1,5 @@
+class User_Student:
+    def __init__(self, nome, id_usuario, credito_emprestimo = 60):
+        self.nome = nome; self.id_usuario = id_usuario;
+        self.credito_acesso = True; self.credito_emprestimo = credito_emprestimo; 
+    # inicialmente pode ter acesso aos livros (60 credito)
