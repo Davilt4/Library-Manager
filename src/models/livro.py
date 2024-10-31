4a26560b50708413767037cc709c2a51d9592b0a
oiclai <claradealcantara285@gmail.com>
2024-10-31 15:24:48 -0300

    :construction: attempt fixing bug

--- src/models/livro.py
+++ src/models/livro.py
@@ -13,24 +13,36 @@ class Book:
         self.data_devolucao = None # Achei interessante ter esse atributo para colocar na tabela.
         # inicialmente no sistema o livro nao foi emprestado (sem user), nao apresentando data de emprestimo (ele vai ganhando um historico de data de emprestimos)        
 
-    def emprestado(self, user, data_emprestimo): 
-        if not isinstance(user, User_Student) or not hasattr(user, "id_usuario"):
+
+
+    def emprestado(self, cpf_usuario, data_emprestimo): 
+        if not isinstance(cpf_usuario, User_Student) or not hasattr(cpf_usuario, "cpf_usuario"):
+
             raise TypeError("O parâmetro 'user' deve ser uma instância de User_Student com o atributo 'id_usuario'.")
-        self.user_id = user.id_usuario
-        self.user = user 
+
+
+        self.cpf_usuario = cpf_usuario.cpf_usuario
+        # acessando o cpf_usuario da class e declarando que ele tem um valor de (cpf_usuario.cpf_usuario) que seria 
         if not self.disponivel:
             print("ERROR: Livro já emprestado.")
             return # impossibilita emprestimo se ja estiver emprestado
-        self.data_emprestimo = datetime.strptime(data_emprestimo, "%d/%m/%Y");
+        try:
+            self.data_emprestimo = datetime.strptime(data_emprestimo, "%d/%m/%Y") # se as datas n estiverem no formato que deveriam
+        except ValueError:
+            raise ValueError("Data de empréstimo deve estar no formato dd/mm/yyyy.")
+
         self.disponivel = False; # livro emprestado
 
-    def devolvido(self, data_devolucao):
-        if self.data_emprestimo is None:
+
+
+
+    def devolvido(self, data_devolucao, cpf_usuario):
+        if self.data_emprestimo or cpf_usuario is None:
             print("ERROR: esse não foi o livro emprestado.")
-            return
+            return # sai do metodo
         self.data_devolucao = datetime.strptime(data_devolucao, "%d/%m/%Y")
         self.maxdata_devolucao = self.data_emprestimo + timedelta(days=30)
 
-        if self.data_devolucao > self.maxdata_devolucao and self.user: 
+        if self.data_devolucao > self.maxdata_devolucao and self.cpf_usuario: 
             self.user.credito_emprestimo -= 1  # reduz o credito
-        self.disponivel = True; self.user = None; # remove a referencia do usuario ja q o livro ja foi devolvido
+        self.disponivel = True; self.cpf_usuario = None; # remove a referencia do usuario ja q o livro ja foi devolvido
--- src/models/usuario.py
+++ src/models/usuario.py
@@ -1,8 +1,8 @@
 # user id = cpf -> ate poderia ser um id gerado, mas por agora fica mais simples com o proprio user cpf
 class User_Student:
-    def __init__(self, nome, id_usuario, credito_emprestimo = 60):
+    def __init__(self, nome, cpf_usuario, credito_emprestimo = 60):
         self.nome = nome
-        self.id_usuario = id_usuario
+        self.cpf_usuario = cpf_usuario
         self.credito_acesso = True
         self.credito_emprestimo = credito_emprestimo; 
     # inicialmente pode ter acesso aos livros (60 credito)
